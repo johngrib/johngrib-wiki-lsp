87827e35fe6fd12a4a2f83a9bb5c645428e707cd
JohnGrib <johngrib82@gmail.com>
2023-03-27 23:46:25 +0900

    파일 이름이 툴팁으로 표시되게 한다

--- index.js
+++ index.js
@@ -9,47 +9,66 @@ const {
 const {TextDocument} = require('vscode-languageserver-textdocument')
 const fs = require('fs');
 
+const { readFirstFiveLines, readMarkdownMetadataSync } = require('./metadata');
+
 const currentDirectory = process.cwd();
+
+const getFileAddress = (linkString) => {
+    const fileAddress = linkString.startsWith('/')
+        ? `${currentDirectory}/_wiki${linkString}.md`
+        : `${currentDirectory}/_wiki/${linkString}.md`;
+    return fileAddress
+}
 
 const getWikiLinks = (text) => {
     const regex = /\[\[([^\]]+)\]\]/g;
     const results = []
     regex.lastIndex = 0
     while ((matches = regex.exec(text)) && results.length < 100) {
-        const linkString = matches[1];
-
-        const fileAddress = linkString.startsWith('/')
-            ? `${currentDirectory}/_wiki${linkString}.md`
-            : `${currentDirectory}/_wiki/${linkString}.md`;
-
-        const isExistFile = fs.existsSync(fileAddress);
-
-        if ( ! isExistFile) {
-            results.push({
-                value: matches[0],
-                index: matches.index,
-            })
-        }
+        results.push({
+            index: matches.index,
+            value: matches[0],
+            text: matches[1],
+        })
     }
     return results
 }
 
 const getDiagnostics = (textDocument) => {
-    const text = textDocument.getText();
-    const warningList = getWikiLinks(text);
+  const text = textDocument.getText();
+  const warningList = getWikiLinks(text);
 
-    return warningList.map(function ({index, value}) {
-        return {
-            severity: DiagnosticSeverity.Warning,
-            range: {
-                start: textDocument.positionAt(index),
-                end: textDocument.positionAt(index + value.length),
-            },
-            message: `${value} is invalid link.`,
-            source: 'JohnGrib Wiki LSP',
-        }
-    })
-}
+  const results = warningList.map(function ({ index, value, text }) {
+    const fileAddress = getFileAddress(text);
+    const isExistFile = fs.existsSync(fileAddress);
+
+    if (!isExistFile) {
+      return {
+        severity: DiagnosticSeverity.Warning,
+        range: {
+          start: textDocument.positionAt(index),
+          end: textDocument.positionAt(index + value.length),
+        },
+        message: `${value} is invalid link.`,
+        source: "JohnGrib Wiki LSP",
+      };
+    }
+
+    const meta = readMarkdownMetadataSync(fileAddress);
+
+    return {
+      severity: DiagnosticSeverity.Information,
+      range: {
+        start: textDocument.positionAt(index),
+        end: textDocument.positionAt(index + value.length),
+      },
+      message: meta.title,
+      source: "JohnGrib Wiki LSP",
+    };
+  });
+
+  return results;
+};
 
 const connection = createConnection()
 const documents = new TextDocuments(TextDocument)
