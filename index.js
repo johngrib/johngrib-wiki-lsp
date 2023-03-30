5055ca696225148e1f25c0bc8845ddf71b069ec0
JohnGrib <johngrib82@gmail.com>
2023-03-31 00:10:21 +0900

    위키 문서 자동완성 기능 추가

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const {
     DiagnosticSeverity,
     TextDocuments,
     createConnection,
+    CompletionItemKind,
 } = require('vscode-languageserver')
 const {TextDocument} = require('vscode-languageserver-textdocument')
 const fs = require('fs');
+const path = require('path');
+const DOCUMENT_ROOT_DIR = '_wiki';
 
 const { readFirstFiveLines, readMarkdownMetadataSync } = require('./metadata');
 
@@ -76,12 +79,6 @@ const getDiagnostics = (textDocument) => {
 const connection = createConnection()
 const documents = new TextDocuments(TextDocument)
 
-connection.onInitialize(() => ({
-    capabilities: {
-        textDocumentSync: documents.syncKind,
-    },
-}))
-
 /**
  * 파일이 변경될 때마다 diagnostics를 판별해 그 결과를 클라이언트에 전달한다.
  */
@@ -92,6 +89,55 @@ documents.onDidChangeContent(change => {
     };
     connection.sendDiagnostics(diagnosticsData);
 })
+
+/**
+ * 자동완성 아이템을 수집해 리턴합니다.
+ */
+const getCompletionItems = () => {
+    const markdownFiles = getMarkdownFiles(path.join(currentDirectory, DOCUMENT_ROOT_DIR));
+    const completionItems = markdownFiles.map((file) => {
+        const relativePath = path.relative(path.join(currentDirectory, DOCUMENT_ROOT_DIR), file);
+        const label = relativePath.replace(/\.md$/, '');
+        return {
+            label,
+            kind: CompletionItemKind.File,
+            detail: `Markdown file: ${label}`,
+            documentation: `Link to the file: [[${label}]]`,
+        };
+    });
+
+    return completionItems;
+};
+
+connection.onCompletion((_textDocumentPosition, token) => {
+    return getCompletionItems();
+});
+
+const getMarkdownFiles = (dir, fileList = []) => {
+    const files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            fileList = getMarkdownFiles(filePath, fileList);
+        } else if (path.extname(file) === '.md') {
+            fileList.push(filePath);
+        }
+    });
+
+    return fileList;
+};
+
+connection.onInitialize(() => ({
+    capabilities: {
+        textDocumentSync: documents.syncKind,
+        completionProvider: {
+            triggerCharacters: ['/'],   // 자동완성 트리거 키는 /
+        },
+    },
+}))
 
 documents.listen(connection)
 connection.listen()
