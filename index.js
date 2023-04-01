c4e16621e2df1e8e4e6549c12a20c677fdba6bab
JohnGrib <johngrib82@gmail.com>
2023-04-01 13:51:50 +0900

    자동완성 기능 리팩토링

--- index.js
+++ index.js
@@ -4,24 +4,23 @@
 const {
     TextDocuments,
     createConnection,
-    CompletionItemKind,
 } = require('vscode-languageserver')
 const {TextDocument} = require('vscode-languageserver-textdocument')
-const fs = require('fs');
-const path = require('path');
-const DOCUMENT_ROOT_DIR = '_wiki';
 
 const DIAGNOSTICS = {
     link: require('./src/diagnostics/link'),
 }
+const COMPLETION = {
+    link: require('./src/completion/link'),
+}
 
-const currentDirectory = process.cwd();
 const connection = createConnection()
 const documents = new TextDocuments(TextDocument)
 
 const CTX = {
     connection,
     rootDirectory: process.cwd(),
+    documentRootName: '_wiki',
 }
 
 const getDiagnostics = (textDocument) => {
@@ -30,9 +29,7 @@ const getDiagnostics = (textDocument) => {
     return linkDiagnostics;
 };
 
-/**
- * 파일이 변경될 때마다 diagnostics를 판별해 그 결과를 클라이언트에 전달한다.
- */
+/** 파일이 변경될 때마다 diagnostics를 판별해 그 결과를 클라이언트에 전달한다. */
 documents.onDidChangeContent(change => {
     const diagnosticsData = {
         uri:         change.document.uri,
@@ -41,46 +38,12 @@ documents.onDidChangeContent(change => {
     connection.sendDiagnostics(diagnosticsData);
 })
 
-/**
- * 자동완성 아이템을 수집해 리턴합니다.
- */
-const getCompletionItems = () => {
-    const markdownFiles = getMarkdownFiles(path.join(currentDirectory, DOCUMENT_ROOT_DIR));
-    const completionItems = markdownFiles.map((file) => {
-        const relativePath = path.relative(path.join(currentDirectory, DOCUMENT_ROOT_DIR), file);
-        const label = relativePath.replace(/\.md$/, '');
-        return {
-            label,
-            kind: CompletionItemKind.File,
-            detail: `Markdown file: ${label}`,
-            documentation: `Link to the file: [[${label}]]`,
-        };
-    });
-
-    return completionItems;
-};
-
+/* 자동완성 목록 제공. */
 connection.onCompletion((_textDocumentPosition, token) => {
-    return getCompletionItems();
+    return COMPLETION.link.getList(CTX);
 });
 
-const getMarkdownFiles = (dir, fileList = []) => {
-    const files = fs.readdirSync(dir);
-
-    files.forEach((file) => {
-        const filePath = path.join(dir, file);
-        const stat = fs.statSync(filePath);
-
-        if (stat.isDirectory()) {
-            fileList = getMarkdownFiles(filePath, fileList);
-        } else if (path.extname(file) === '.md') {
-            fileList.push(filePath);
-        }
-    });
-
-    return fileList;
-};
-
+/* LSP 초기화. */
 connection.onInitialize(function() {
     connection.window.showInformationMessage('JohnGrib Wiki LSP is running...');
     return {
